4f4c6eec50ebc85324b08c9c6643428d8eefd41a
Nick Brenner <nlb74@cornell.edu>
2026-02-19 15:58:58 -0500

    Add Bazel building functionality
    Add a BUILD file which specifies the
    associated plugin source code files,
    along with junit_tests. The plugin name
    is set to "multianchor_comment".

---
+++ README.md
@@ -0,0 +1,3 @@
+# Building
+
+To build the plugin, run: bazel build //plugins/multianchor_comment:multianchor_comment
--- src/test/java/com/googlesource/gerrit/plugins/multianchorcomment/MultianchorCommentIT.java
+++ src/test/java/com/googlesource/gerrit/plugins/multianchorcomment/MultianchorCommentIT.java
@@ -1,4 +1,4 @@
-package com.googlesource.gerrit.plugins.hooks;
+package com.googlesource.gerrit.plugins.multianchorcomment;
 
 import com.google.gerrit.acceptance.LightweightPluginDaemonTest;
 import com.google.gerrit.acceptance.NoHttpd;
@@ -6,9 +6,13 @@ import com.google.gerrit.acceptance.TestPlugin;
 import org.junit.Test;
 
 @NoHttpd
-@TestPlugin(name = "hooks", sysModule = "com.googlesource.gerrit.plugins.hooks.PluginModule")
-public class HooksIT extends LightweightPluginDaemonTest {
+@TestPlugin(
+    name = "multianchor_comment",
+    sysModule = "com.googlesource.gerrit.plugins.multianchorcomment.PluginModule"
+)
+public class MultiAnchorCommentIT extends LightweightPluginDaemonTest {
 
   @Test
-  public void doNothing() throws Exception {}
+  public void pluginLoads() throws Exception {
+  }
 }
